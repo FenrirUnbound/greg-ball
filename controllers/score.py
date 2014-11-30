606fb05badb1058ef7cc6745a600d5aba167ada0
Darren M <aeneascorrupt@gmail.com>
2014-11-29 21:24:45 -0800

    endpoint for setting a specific game resource

--- controllers/score.py
+++ controllers/score.py
@@ -15,3 +15,16 @@ class ScoreHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.headers['Access-Control-Allow-Origin'] = '*'
         self.response.write(json.dumps(result))
+
+    def put(self, year, week, game):
+        year = int(year)
+        week = int(week)
+        game = int(game)
+        data = json.loads(self.request.POST['game'])
+
+        score = Score()
+        result = score.save(data=data, game_id=game, week=week, year=year)
+
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.write(json.dumps(result))
--- models/score.py
+++ models/score.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import ndb
 
+from models.datastore.score_ds import ScoreModel
+
 class Score(object):
     def __init__(self):
         pass
@@ -9,3 +11,12 @@ class Score(object):
         data = key.get()
 
         return data.to_dict()
+
+    def save(self, data, game_id, week=0, year=0):
+        key = ndb.Key('year', year, 'week', week, 'ScoreModel', game_id)
+        score_data = {'key': key}
+        score_data.update(data)
+
+        ScoreModel(**score_data).put()
+
+        return 1
